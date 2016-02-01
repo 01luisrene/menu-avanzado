29818ec3ac7fab007ffdbeaa7eaa430ccb7f66c5
Luis Rene Mas Mas <01luisrene@gmail.com>
2016-01-31 21:01:32 -0500

    menu web lince
    subiendo archivos

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Menú Web
+# Menú Web Lince
 Este pequeño proyecto fue creado con la única intención de facilitar la implementación del menú de su sitio web.
+
 ## Framework
 
 [Bootstrap](http://getbootstrap.com)
--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
 	//script para colocar el menú estático
   $('#js-contenedor-menu').scrollToFixed();
+
+  $('#js-ico-menu').on('click',function(e){
+    e.preventDefault();
+    $('.main-menu').slideToggle();                 
+  });
+
+	$(window).on('scroll', function() {
+		if($(window).scrollTop() > 1) {
+			$('#js-contenedor-logo').addClass('hide-logo');
+			$('#js-contenedor-ico').addClass('height-contenedor-ico');
+			$('#js-contenedor-ico .ico-menu-center').addClass('hide-logo');
+		} else {
+			$('#js-contenedor-logo').removeClass('hide-logo');
+			$('#js-contenedor-ico').removeClass('height-contenedor-ico');
+			$('#js-contenedor-ico .ico-menu-center').removeClass('hide-logo');
+		}
+	});
 });
