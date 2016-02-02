aa002a618b3df15da79da58786e2d9d126b0e041
Luis Rene Mas Mas <01luisrene@gmail.com>
2016-02-01 20:09:04 -0500

    menú web lince
    archivos menú web lince

--- js/main.js
+++ js/main.js
@@ -9,13 +9,26 @@ $(document).ready(function(){
 
 	$(window).on('scroll', function() {
 		if($(window).scrollTop() > 1) {
-			$('#js-contenedor-logo').addClass('hide-logo');
-			$('#js-contenedor-ico').addClass('height-contenedor-ico');
-			$('#js-contenedor-ico .ico-menu-center').addClass('hide-logo');
+			if($( window ).width() >= 768 && $( window ).width() <= 991){
+				$('#js-contenedor-menu #js-contenedor-logo').addClass('hide-logo');
+			}
+			//$('#js-contenedor-menu #js-contenedor-logo').addClass('hide-logo');
+			//$('#js-contenedor-ico').addClass('height-contenedor-ico');
+			//$('#js-contenedor-ico .ico-menu-center').addClass('hide-logo');
+			if($( window ).width() <= 767){
+				$('#js-contenedor-menu .ico-menu-center').addClass('hide-logo height-menu');
+			}
 		} else {
-			$('#js-contenedor-logo').removeClass('hide-logo');
-			$('#js-contenedor-ico').removeClass('height-contenedor-ico');
-			$('#js-contenedor-ico .ico-menu-center').removeClass('hide-logo');
+			//$('#js-contenedor-menu #js-contenedor-logo').removeClass('hide-logo');
+			//$('#js-contenedor-ico').removeClass('height-contenedor-ico');
+			//$('#js-contenedor-ico .ico-menu-center').removeClass('hide-logo');
+			if($( window ).width() >= 768 && $( window ).width() <= 991){
+				$('#js-contenedor-menu #js-contenedor-logo').removeClass('hide-logo');
+			}
+			if($( window ).width() <= 767){
+				$('#js-contenedor-menu .ico-menu-center').removeClass('hide-logo height-menu');
+			}
 		}
+		
 	});
 });
