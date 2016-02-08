8bfa2f57378dcd419b0af844c230bb5f3932508c
Luis Rene Mas Mas <01luisrene@gmail.com>
2016-02-07 20:53:51 -0500

    menú web lince v. 1.0
    subiendo archivos del menú web lince

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
 $(document).ready(function(){
+	var menu = $('.main-menu');
 	//script para colocar el menú estático
-  $('#js-contenedor-menu').scrollToFixed();
+	$('#js-contenedor-menu').scrollToFixed();
 
-  $('#js-ico-menu').on('click',function(e){
-    e.preventDefault();
-    $('.main-menu').slideToggle();                 
-  });
+	$('#js-ico-menu').on('click',function(e){
+	e.preventDefault();
+	menu.slideToggle();                 
+	});
+
+	$(window).resize(function(event) {
+		var w = $(window).width();
+		if (w > 768 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		};
+	});
 
 	$(window).on('scroll', function() {
 		if($(window).scrollTop() > 1) {
 			if($( window ).width() >= 768 && $( window ).width() <= 991){
 				$('#js-contenedor-menu #js-contenedor-logo').addClass('hide-logo');
 			}
-			//$('#js-contenedor-menu #js-contenedor-logo').addClass('hide-logo');
-			//$('#js-contenedor-ico').addClass('height-contenedor-ico');
-			//$('#js-contenedor-ico .ico-menu-center').addClass('hide-logo');
 			if($( window ).width() <= 767){
 				$('#js-contenedor-menu .ico-menu-center').addClass('hide-logo height-menu');
 			}
-		} else {
-			//$('#js-contenedor-menu #js-contenedor-logo').removeClass('hide-logo');
-			//$('#js-contenedor-ico').removeClass('height-contenedor-ico');
-			//$('#js-contenedor-ico .ico-menu-center').removeClass('hide-logo');
+		} else{
 			if($( window ).width() >= 768 && $( window ).width() <= 991){
 				$('#js-contenedor-menu #js-contenedor-logo').removeClass('hide-logo');
 			}
