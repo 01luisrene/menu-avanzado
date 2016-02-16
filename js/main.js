491434fc330a6a65afbe72df3477bec839b835e5
Luis Rene Mas Mas <01luisrene@gmail.com>
2016-02-15 20:09:49 -0500

    Web Menú Lince v. 1.0
    Subiendo los archivos para la app Web Menú Lince.

--- js/main.js
+++ js/main.js
@@ -1,64 +1,86 @@
 $(document).ready(function(){
-	var menu = $('.box-main-menu');
+	var list_menu = $('.box-main-menu'),
+		menu_fijo = $('#js-contenedor-menu'),
+		menu_offset = menu_fijo.offset();
 	/*
-	Esta línea de código llama a la función de la libreria scrollToFixed
+	Esta función sirve para que cuando el usuario haga click en el botón de Menú Web se despliegue o repliegue.
 	----
-	Sirve para que el menú se quede fijo
-	*/
-	$('#js-contenedor-menu').scrollToFixed();
-
-	/*
-	Esta función sirve para que cuando el usuario haga click en el botón de menú se despliegue o repliegue.
-	----
-	Se podra apreciar en pantallas menores a 768px
+	Solo se podrá apreciar en pantallas menores a 768px
 	*/
 	$('#js-icono-menu-movil').on('click',function(e){
 		e.preventDefault();
-		menu.slideToggle();                 
+		list_menu.slideToggle();                 
 	});
 
-	//Esta función sirve para validar cuando el navegador, es cambiado de tamaño manualmente.
+	//resize sirve para validar cuando el navegador es cambiado de tamaño manualmente.
 	$(window).resize(function() {
-		//esta condición sirve para mostrar el menú si esta oculto.
-		//cuando la pantalla sea mayor a 768px
+		/*
+		esta condición sirve para mostrar el menú si esta oculto.
+		---------
+		cuando la pantalla sea mayor a 768px
+		*/
 		var w = $(window).width();
-		if (w > 768 && menu.is(':hidden')) {
-			menu.removeAttr('style');
+		if (w > 768 && list_menu.is(':hidden')) {
+			list_menu.removeAttr('style');
 		};
 	});
 
-	var scroleo = function(ancho){
-		if($(window).scrollTop() > 1) {
-			if(ancho >= 768 && ancho <= 991){
-				$('#js-contenedor-menu #js-contenedor-logo').addClass('hide-logo');
-			}
-			if(ancho <= 767){
-				$('#js-contenedor-menu .icono-menu-movil-center').addClass('hide-logo height-menu');
-			}
-		} else{
-			if(ancho >= 768 && ancho <= 991){
-				$('#js-contenedor-menu #js-contenedor-logo').removeClass('hide-logo');
-			}
-			if(ancho <= 767){
-				$('#js-contenedor-menu .icono-menu-movil-center').removeClass('hide-logo height-menu');
-			}
-		}
-	};
 
 	$(window).on('scroll', function() {
+		//========== Menú fijo ===========
+		/*
+			Cuando el usuario haga scroll automáticamente el menú web se quedara estático
+		*/
+		if($(window).scrollTop() > menu_offset.top) {
+		  menu_fijo.addClass('menu-fijo');
+		} else {
+		  menu_fijo.removeClass('menu-fijo');
+		}
+
 		var ancho = $(window).width();
 		scroleo(ancho)
-		//Esta función sirve para validar cuando el navegador, es cambiado de tamaño manualmente. 
+
+		//resize sirve para validar cuando el navegador es cambiado de tamaño manualmente. 
 		$(window).resize(function() {
 			var ancho = $(window).width();
 			scroleo(ancho);
 		});
 	});
 
-	/* No tomar importancia a estos scripts*/
+	var scroleo = function(ancho){
+		if($(window).scrollTop() > 1) {
+			//Esta condición se cumple si la pantalla es mayor o igual a 768px y menor o igual a 991px
+			if(ancho >= 768 && ancho <= 991){
+				/**
+				Si la condición se cumple agregamos la clase hide-logo
+				**/
+				$('#js-contenedor-menu #js-contenedor-logo-nameapp').addClass('hide-logo');
+			}
+			/*
+			Esta condición se cumple si el contenedor con la clase contenedor-iconos tiene al lado clase icono-menu-movil-center
+			--------
+			y la pantalla sea menor o igual a 767
+			*/
+			if(ancho <= 767){
+				/**
+				Si la condición se cumple agregamos las clases hide-logo height-menu
+				**/
+				$('#js-contenedor-menu .icono-menu-movil-center').addClass('hide-logo height-menu');
+			}
+		} else{
+			/**
+				Si no se cumple removemos la clase hide-logo
+			**/
+			if(ancho >= 768 && ancho <= 991){
+				$('#js-contenedor-menu #js-contenedor-logo-nameapp').removeClass('hide-logo');
+			}
 
-	//https://highlightjs.org/
-	 $('pre code').each(function(i, block) {
-	    hljs.highlightBlock(block);
-	 });
+			/**
+				Si no se cumple removemos las clases hide-logo y height-menu
+			**/
+			if(ancho <= 767){
+				$('#js-contenedor-menu .icono-menu-movil-center').removeClass('hide-logo height-menu');
+			}
+		}
+	};
 });
